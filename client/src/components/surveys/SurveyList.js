ea2cf9bbd54f560218d1859cb2f036477efcaa2f
kelly <kellyrose.evergreen@gmail.com>
2017-08-13 11:58:28 +0900

    5th : 193 (fixed SurveyList in client)

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -3,10 +3,35 @@ import {connect} from 'react-redux';
 import {fetchSurveys} from "../../actions";
 
 class SurveyList extends Component {
+    componentDidMount(){
+        this.props.fetchSurveys();
+    }
+    renderSurveys() {
+        return this.props.surveys.reverse().map(survey => {
+            return (
+                <div className="card darken-1" key={survey._id}>
+                    <div className="card-content">
+                        <span className="card-title">{survey.title}</span>
+                        <p>
+                            {survey.body}
+                        </p>
+                        <p className="right">
+                            Sent On: {new Date(survey.dateSent).toLocaleDateString()}
+                        </p>
+                    </div>
+                    <div className="card-action">
+                        <a>Yes: {survey.yes}</a>
+                        <a>No: {survey.no}</a>
+                    </div>
+                </div>
+            );
+        });
+    }
+
     render() {
         return (
            <div>
-               SurveyList
+               {this.renderSurveys()}
            </div>
         );
     }
@@ -16,4 +41,4 @@ function mapStateProps({surveys}) {
     return {surveys};
 }
 
-export default connect(mapStateProps,fetchSurveys)(SurveyList);
+export default connect(mapStateProps,{fetchSurveys})(SurveyList);
