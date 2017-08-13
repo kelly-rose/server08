7cf1e65d61e80f3ca252a27e83a0f71be45d4ab5
kelly <kellyrose.evergreen@gmail.com>
2017-08-13 11:39:33 +0900

    3rd : 191 (fixed action and reducers in client)

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import {FETCH_USER} from "./types";
+import {FETCH_USER,FETCH_SURVEYS} from "./types";
 
 // export const fetchUser = () =>{
 //     return function(dispatch) {
@@ -28,3 +28,8 @@ export const submitSurvey = (values,history) => async dispatch => {
     dispatch({ type: FETCH_USER, payload: res.data });
 };
 
+export const fetchSurveys = () => async dispatch => {
+    const res = await axios.get('/api/surveys');
+
+    dispatch({ type: FETCH_SURVEYS, payload: res.data });
+};
